a5eb4a97f38aea49efabcc4a2d84f982b12874c2
Kayla Hansen <shiverr@gmail.com>
2017-10-12 03:02:02 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence)
   new_sentence = sentence.split
   new_sentence.collect do |reversed|
-    sentence << reversed.reverse
+    reversed.reverse
+    sentence
   end
 end
