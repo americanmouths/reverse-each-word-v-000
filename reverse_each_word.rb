4a28dd6a9222b07ce4194e1abb43b74458c60524
Kayla Hansen <shiverr@gmail.com>
2017-10-12 02:40:06 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
-  new_array = sentence.split(" ")
+  new_array = sentence.split
   new_sentence = []
   new_array.each do |reversed|
     new_sentence << reversed.reverse
